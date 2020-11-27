efe01a35c887bab40070ce057d7603365c2fce96
Cookie <Cookie@inventec.com>
2020-11-27 16:39:39 +0800

    url-loader 根据limit字节打包成base64格式

--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -15,8 +15,16 @@ module.exports = {
       // 图片后缀
       test: /\.(png|jpg|gif)$/,
       use: {
-        // 使用插件
-        loader: 'file-loader',
+        // 使用插件是file-loader，url-loader会根据limit大小决定是否打包在js文件中成base64，从而减少http 请求
+        loader: 'url-loader',
+        options:{
+          // 设置打包后图片名和扩展后缀
+          name: '[name].[ext]',
+          // 图片输出路径
+          outputPath: '/images',
+          // 10240 = 10k,定制大小是否打包base64
+          limit: 102400
+        }
       }
     }]
   },
